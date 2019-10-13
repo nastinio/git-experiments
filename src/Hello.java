e576f908ac15be7ab6f9f0852cfef455b4bf170c
Бессарабова Анастасия <abessarabova@metaprime.ru>
2019-10-13 20:38:41 +0300

    Новый старт для экспериментов с ветками

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Йоу!");
-    System.out.println("Дубль два. Старт 1");
+    System.out.println("Дубль три. Старт 1");
   }
 }
