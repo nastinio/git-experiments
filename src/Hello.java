3090b423494754f43b548d0062c5e299fb8bd01b
Бессарабова Анастасия <abessarabova@metaprime.ru>
2019-10-13 22:07:09 +0300

    m1

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     System.out.println("Йоу!");
     System.out.println("Дубль три. Старт 1");//
     System.out.println("Потенциальная помеха фиче (ветке с ней)");
-    System.out.println("Повторенье - мать ученья");
+    //System.out.println("Повторенье - мать ученья");
    // System.out.println("Boo");
   }
 }
