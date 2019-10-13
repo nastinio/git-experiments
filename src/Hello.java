143cc8b725eff705789b6ae6941adde27d8f9d32
Бессарабова Анастасия <abessarabova@metaprime.ru>
2019-10-13 20:41:37 +0300

    Что-то новое на master

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Йоу!");
     System.out.println("Дубль три. Старт 1");
+    System.out.println("Потенциальная помеха фиче (ветке с ней)");
   }
 }
