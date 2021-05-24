34d691b82c13ae19391316f0db8aed11a4f76bff
Бессарабова Анастасия <abessarabova@metaprime.ru>
2021-05-24 13:04:54 +0300

    Тест rebase

--- src/Hello.java
+++ src/Hello.java
@@ -1,18 +1,30 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Йоу!");
 
-    Date date = new Date();
-    Date dateEnd = date;
+    /*System.out.println(resList.get(0));
 
-    check(date, dateEnd);
+    Date[] dates = {null, date1, date2, date3, null};
+    for (Date date : dates) {
+      if (date != null){
+        if (datemin == null || date.before(datemin)) {
+          datemin = date;
+        }
+      }
+    }
 
-  }
-  public static void check(Date date, Date dateEnd) {
-    System.out.println(date == null ? dateEnd != null : (dateEnd == null || !dateEnd.equals(date)));
+    System.out.println(datemin);*/
+    /*// Прочекать, сколько тредов создастся
+    Collection<Integer> docs = new ArrayList<>();
+    for (int i = 0; i < 1000; i++) {
+      docs.add(i);
+    }
+
+    // Посчитать сумму всех элементов прокручивающихся в потоке
+    ExecutorService service = Executors.newFixedThreadPool(5);
+    // По сути execute на вход получает поток, который обрабатывает
+    docs.forEach(doc -> service.execute(new SomeTask(doc)));*/
+
   }
 
 
